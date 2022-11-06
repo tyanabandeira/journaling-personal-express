5105a4661e3dceba04eb54f1c0aae7fd55904f7f
tyana bandeira <tyanabandeira1@gmail.com>
2022-11-06 18:36:13 -0500

    first commit

---
+++ public/main.js
@@ -0,0 +1,109 @@
+var faceSmile = document.getElementsByClassName("fa-face-smile");
+var thumbDown = document.getElementsByClassName("fa-thumbs-down")
+var trash = document.getElementsByClassName("fa-trash");
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Array.from(thumbUp).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         const intentions = this.parentNode.parentNode.childNodes[5].innerText
+//         const grateful = this.parentNode.parentNode.childNodes[5].innerText
+//         const affirmations = this.parentNode.parentNode.childNodes[5].innerText
+//         const text = this.parentNode.parentNode.childNodes[5].innerText
+//         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//         fetch('messages', {
+//           method: 'put',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg,
+//             'thumbUp':thumbUp,
+//             'grateful': grateful,
+//             'affirmations': affirmations,
+//             'text': text
+//           })
+//         })
+//         .then(response => {
+//           if (response.ok) return response.json()
+//         })
+//         .then(data => {
+//           console.log(data)
+//           window.location.reload(true)
+//         })
+//       });
+// });
+
+// Array.from(thumbDown).forEach(function(element) {
+//   element.addEventListener('click', function(){
+//     const name = this.parentNode.parentNode.childNodes[1].innerText
+//     fetch('messages', {
+//       method: 'put',
+//       headers: {'Content-Type': 'application/json'},
+//       body: JSON.stringify({
+//         'name': name,
+//         'msg': msg,
+//       })
+//     })
+//     .then(response => {
+//       if (response.ok) return response.json()
+//     })
+//     .then(data => {
+//       console.log(data)
+//       window.location.reload(true)
+//     })
+//   });
+// });
+
+
+// Array.from(faceSmile).forEach(function(element) {
+//   element.addEventListener('click', function(){
+//     const name = this.parentNode.parentNode.childNodes[1].innerText
+//     fetch('messages', {
+//       method: 'put',
+//       headers: {'Content-Type': 'application/json'},
+//       body: JSON.stringify({
+//         'name': name,
+//         'msg': msg,
+//       })
+//     })
+//     .then(response => {
+//       if (response.ok) return response.json()
+//     })
+//     .then(data => {
+//       console.log(data)
+//       window.location.reload(true)
+//     })
+//   });
+// });
+
+
+
